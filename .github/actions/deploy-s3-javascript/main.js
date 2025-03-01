0b3fd14a0fb3063db0d1b5c8318a2eb3bcd77e95
srini <srini@dev.com>
2025-03-01 17:35:08 +0000

    add output for custom js action deploy

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -17,6 +17,8 @@ function run() {
     const s3Uri = `s3://${bucket}`
     exec.exec(`aws s3 sync ${distFolder} ${s3Uri} --region ${bucketRegion}`) // executes the passed command as bash on the runner
     
+    const websiteUrl = `http://${bucket}.s3-website-${bucketRegion}.amazonaws.com`
+    core.setOuput('website-url', websiteUrl) // set output
     core.notice('Hello from my custom JavaScript action'); // prints the message to log in GHA
 }
 
