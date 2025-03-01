f24faa9bed1b3ba176eb1bb0e7c7857612195ad2
srini <srini@dev.com>
2025-03-01 17:28:06 +0000

    add custom js action deploy action

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -8,7 +8,16 @@ function run() {
     npm install @actions/core @actions/github @actions/exec
   */
 
-  core.notice('Hello from my custom JavaScript action') // prints the message to log in GHA
+    // 1) Get some input values
+    const bucket = core.getInput('bucket', {required: true});
+    const bucketRegion = core.getInput('bucket-region', {required: false});
+    const distFolder = core.getInput('dist-folder', {required: true});
+
+    //2) Upload the files
+    const s3Uri = `s3://${bucket}`
+    exec.exec(`aws s3 sync ${distFolder} ${s3Uri} --region ${bucketRegion}`) // executes the passed command as bash on the runner
+    
+    core.notice('Hello from my custom JavaScript action'); // prints the message to log in GHA
 }
 
 run();
